f9f448ce6e680bfb44a1ea765535d10358768d31
ericGutierrezM <gh.ericgutierrez@gmail.com>
2025-11-08 09:36:19 +0100

    run code and save model

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+import joblib
 
 # Load and clean data #
 
@@ -43,20 +44,34 @@ def fit_model(X_train, X_test, y_train):
     sc = StandardScaler()
     X_train_std = sc.fit_transform(X_train)
     X_test_std  = sc.transform(X_test)
-    modelLogistic = LogisticRegression(penalty=None, solver='lbfgs', max_iter=2000, random_state=102)
-    modelLogistic.fit(X_train_std, y_train)
-    train_y_pred = modelLogistic.predict_proba(X_train_std)[:, 1]
-    test_y_pred = modelLogistic.predict_proba(X_test_std)[:, 1]
+    model_logistic = LogisticRegression(penalty=None, solver='lbfgs', max_iter=2000, random_state=102)
+    model_logistic.fit(X_train_std, y_train)
+    train_y_pred = model_logistic.predict_proba(X_train_std)[:, 1]
+    test_y_pred = model_logistic.predict_proba(X_test_std)[:, 1]
     updated_X_train = X_train
     updated_X_train['prediction'] = train_y_pred
     updated_X_test = X_test
     updated_X_test['prediction'] = test_y_pred
-    return updated_X_train, updated_X_test
+    return updated_X_train, updated_X_test, model_logistic
 
 
-# Compute metrics #
+# Run model #
 
-def updated_roc_auc_score(updated_X_train, updated_X_test, y_train, y_test):
-    train_roc_auc = roc_auc_score(y_train, updated_X_train['prediction'])
-    test_roc_auc = roc_auc_score(y_test, updated_X_test['prediction'])
-    return train_roc_auc, test_roc_auc
+data = load_data('sample_diabetes_mellitus_data.csv')
+X_train, X_test, y_train, y_test = updated_train_test_split(data, 'diabetes_mellitus')
+X_train, y_train = remove_nan(X_train, y_train, ['age','gender','ethnicity'])
+X_test, y_test = remove_nan(X_test, y_test, ['age','gender','ethnicity'])
+X_train = fill_nan_mean(X_train, ['height', 'weight'])
+X_test = fill_nan_mean(X_test, ['height', 'weight'])
+X_train = generate_dummies(X_train, ['ethnicity'])
+X_test = generate_dummies(X_test, ['ethnicity'])
+X_train = binary_encoding_gender(X_train)
+X_test = binary_encoding_gender(X_test)
+X_train = X_train[['age', 'height', 'weight', 'aids', 'cirrhosis', 'hepatic_failure','immunosuppression','leukemia','lymphoma','solid_tumor_with_metastasis']]
+X_test = X_test[['age', 'height', 'weight', 'aids', 'cirrhosis', 'hepatic_failure','immunosuppression','leukemia','lymphoma','solid_tumor_with_metastasis']]
+updated_X_train, updated_X_test, model_logistic = fit_model(X_train, X_test, y_train)
+
+
+# Save model #
+
+joblib.dump(model_logistic, 'model_logistic.joblib')
